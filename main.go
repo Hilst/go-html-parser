e152a0752642bdada6f06ad3d4a9e77a496a7864
Hilst <hilstfelipe@gmail.com>
2023-09-17 22:45:08 -0300

    service

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	HTMLtmpl "html/template"
 	"os"
 )
@@ -27,4 +28,36 @@ func main() {
 	all := HTMLtmpl.Must(HTMLtmpl.New("ALL").ParseGlob("./templates/**/*.html"))
 	println(all.DefinedTemplates())
 	all.ExecuteTemplate(os.Stdout, "MAIN", data)
+}
+
+type IService interface {
+	RequestData(path string) map[string]interface{}
+	RequestLayout(layoutName string) string
+}
+
+type Service struct {
+	dataPath   string
+	layoutPath string
+}
+
+func (s Service) RequestData(path string) map[string]interface{} {
+	data, err := os.ReadFile(s.dataPath + path)
+	if err != nil {
+		panic("CANT READ DATA FILE")
+	}
+	var result map[string]interface{}
+	err = json.Unmarshal(data, &result)
+	if err != nil {
+		panic("CANT UNMARSHAL DATA")
+	}
+	return result
+}
+
+func (s Service) RequestLayout(layoutName string) string {
+	data, err := os.ReadFile(s.layoutPath + layoutName)
+	if err != nil {
+		panic("CANT READ LAYOUT FILE")
+	}
+	layout := string(data)
+	return layout
 }
