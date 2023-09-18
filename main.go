e83b60d4e940e1739b9c5cd401da7078c30fc813
Hilst <hilstfelipe@gmail.com>
2023-09-17 23:02:13 -0300

    separate service

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	HTMLtmpl "html/template"
 	"os"
 )
@@ -22,37 +21,4 @@ func main() {
 	all.New("LAYOUT").Parse(layout)
 	// BUILD LAYOUT INTO OUTPUT
 	all.ExecuteTemplate(os.Stdout, "MAIN", data)
-}
-
-// SERVICE
-type IService interface {
-	RequestData(path string) map[string]interface{}
-	RequestLayout(layoutName string) string
-}
-
-type Service struct {
-	dataPath   string
-	layoutPath string
-}
-
-func (s Service) RequestData(path string) map[string]interface{} {
-	data, err := os.ReadFile(s.dataPath + path)
-	if err != nil {
-		panic("CANT READ DATA FILE")
-	}
-	var result map[string]interface{}
-	err = json.Unmarshal(data, &result)
-	if err != nil {
-		panic("CANT UNMARSHAL DATA")
-	}
-	return result
-}
-
-func (s Service) RequestLayout(layoutName string) string {
-	data, err := os.ReadFile(s.layoutPath + layoutName)
-	if err != nil {
-		panic("CANT READ LAYOUT FILE")
-	}
-	layout := string(data)
-	return layout
 }
