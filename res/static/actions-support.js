33ebad7de4050bed58a7902be295b89b74ffd7aa
Hilst <hilstfelipe@gmail.com>
2023-11-13 15:43:58 -0300

    added JSDoc to actions support js

--- res/static/actions-support.js
+++ res/static/actions-support.js
@@ -1,7 +1,19 @@
+/**
+ * Logs a text with the color passed
+ * @param {string} color 
+ * @param {string} text 
+ */
 window.customLog = function(color, text) {
     console.log(`%c${text}`, `color:${color}`)
 }
 
+/**
+ * Change page from currentactive to nextactive
+ * making the element with id page_currentactive hidden
+ * and the element with id page_nextactive not hidden
+ * @param {string} currentactive 
+ * @param {string} nextactive 
+ */
 window.pagechange = function(currentactive, nextactive) {
     const p = "page_" + currentactive
     const n = "page_" + nextactive
