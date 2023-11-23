7bea8b92bdc61019936cbd8d87269650410ba8e6
Hilst <hilstfelipe@gmail.com>
2023-11-23 14:30:40 -0300

    air supp on run locally

--- runlocally/start.sh
+++ runlocally/start.sh
@@ -1,2 +1,9 @@
 python3 ./scripts/local.py
-docker-compose -f ../docker-compose.yml up
+if [[ $1 == "airmode" ]]
+then
+        export $(egrep -v ‘^#’ ../env/.airenv | xargs)
+        cd ../src/
+        $(go env GOPATH)/bin/air
+else
+    docker-compose -f ../docker-compose.yml up
+fi
